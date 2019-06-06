e7d4088ab2cda323dbd1064780f71d67767888be
Onesimus Addo Appiah <38097150+draddo11@users.noreply.github.com>
2019-06-06 07:28:31 +0100

    uploaded files

---
+++ build/precache-manifest.efcd2bdcc51ed8eafc4ea4bad0cb64fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ce33ff2f4115dd6fa74b7ab07b54f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e83944b3685ff22b95b",
+    "url": "/static/css/main.10d77a9c.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "5e83944b3685ff22b95b",
+    "url": "/static/js/main.3054766b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
